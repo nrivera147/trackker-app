96badc057042183e271e1302560dc4272a807e6b
nrivera147 <riveranatalia147@gmail.com>
2025-03-19 13:16:35 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TrackkerApp from './components/TrackkerApp';
+
+function App() {
+  return (
+    <div className="App">
+      <TrackkerApp />
+    </div>
+  );
+}
+
+export default App;
